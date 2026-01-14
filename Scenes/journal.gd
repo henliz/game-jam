2d5871b9e67e0d18c2c5b95c40649993e502ce03
Anas-Zakaria Altundag <anas.altundag@gmail.com>
2026-01-14 17:44:07 +0100

    journal tab

--- Scenes/journal.gd
+++ Scenes/journal.gd
@@ -7,6 +7,10 @@ var opened_position = 500.0
 var closed_position = 1200.0
 var target_position = closed_position
 
+const DIARY = preload("uid://ddw6so5jr38ps")
+const BLUEPRINT = preload("uid://da253rmeoaxdx")
+
+var current_tab = "diary"
 
 func _process(_delta: float) -> void:
 	position.y = lerp(position.y,target_position,lerp_speed)
@@ -16,3 +20,11 @@ func _process(_delta: float) -> void:
 		else:
 			target_position = opened_position
 		open = !open
+		
+	if open and Input.is_action_just_pressed("switch_tab"):
+		if current_tab == "diary":
+			texture = BLUEPRINT
+			current_tab = "blueprint"
+		else:
+			texture = DIARY
+			current_tab = "diary"
