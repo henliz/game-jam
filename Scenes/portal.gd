b57b562e83b831878401c459835eb7ac9044b0af
INVNTIV <invntiv@gmail.com>
2026-01-23 22:49:53 -0600

    Scene transition setup

--- Scenes/portal.gd
+++ Scenes/portal.gd
@@ -1,9 +1,29 @@
 extends Area3D
 
 @onready var connect_portal: Area3D = get_node("/root/World/Floor4/portal")
+@onready var connect_portal3: Node = get_node("/root/World/Floor3/portal")
+
 @onready var game_state = get_node("/root/GameState")
 
+# TODO
+# Cutscene sequence =>
+# Determine which floor the player should be going to.
+# That tells us which sequence video and title to display
+# 
 func _on_body_entered(body: Node3D) -> void:
-	if body.name == "Player" and game_state.is_floor_unlocked(2):
-		var destination = connect_portal.global_transform.origin
-		body.global_transform.origin = destination
+	if body.name == "Player" :
+		if game_state.is_floor_unlocked(4):
+			# GameStateManager.cutsceneExternal(body, "Floor 4 - The Lantern")
+			var destination = connect_portal.global_transform.origin
+			body.global_transform.origin = destination
+		if game_state.is_floor_unlocked(3):
+			# GameStateManager.cutsceneExternal(body, "Floor 3 - The Laboratory")
+			var destination = connect_portal.global_transform.origin
+			body.global_transform.origin = destination
+		elif game_state.is_floor_unlocked(2):
+			# GameStateManager.cutsceneExternal(body. "Floor 2 - The Library")
+			var destination = connect_portal3.global_transform.origin
+			body.global_transform.origin = destination
+		else:
+			return
+		
